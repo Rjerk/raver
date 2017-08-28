6eabac758fd895049232a61fb61eda25d35e79dc
Rjerk <460139918@qq.com>
2017-08-28 18:20:13 +0800

    httpservice

---
+++ HTTPService.h
@@ -0,0 +1,39 @@
+#ifndef HTTP_SERVICE_H
+#define HTTP_SERVICE_H
+
+#include "noncopyable.h"
+#include <functional>
+#include <string>
+
+namespace raver {
+
+class ServiceManager;
+class HTTPClientConnection;
+class HTTPRespond;
+
+using ConnectCallback = std::function<void (HTTPClientConnection*)>;
+using RespondCallback = std::function<void (HTTPRespond* )>;
+
+class HTTPService : noncopyable {
+public:
+    HTTPService(int port, ServiceManager* sm); // serve in a port and cooperate with the manager.
+
+    ~HTTPService();
+
+    void stop(); // tell manager I'm done.
+
+    void asyncConnect(const std::string& host, int port, const ConnectCallback& cb);
+
+    void connect(const std::string& host, int port, HTTPClientConnection** conn);
+
+private:
+    void accept(int port);
+
+private:
+    ServiceManager* manager_;
+
+};
+
+}
+
+#endif
