6574f46a4ee03f69a75a3155635ea32358c06b31
Rjerk <460139918@qq.com>
2017-08-28 18:19:55 +0800

    main

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "ServiceManager.h"
+#include "HTTPService.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    if (argc != 3) {
+        std::cerr << "Usage: ./raver <port> <num-thread>" << std::endl;
+        exit(1);
+    }
+
+    int port = atoi(argv[1]);
+    int num_thread = atoi(argv[2]);
+
+    raver::ServiceManager manager(num_thread);
+    raver::HTTPService http_serv(port, &manager);
+
+    manager.run();
+
+}
